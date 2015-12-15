68d3ab941fb195a50559dbae1580499727f634e7
Leo Dutra <leodutra.br@gmail.com>
2015-12-15 02:39:25 -0200

    Merging ISO4717 into ISO3361

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 import ISO3361CountryScrapper from './lib/ISO3361CountryScrapper'
 import ISO4717CurrencyScrapper from './lib/ISO4717CurrencyScrapper'
-import ISO3361_ISO4717_Merger from './lib/ISO3361_ISO4717_Merger'
+import ISO4717_Into_ISO3361 from './lib/mergers/ISO4717_Into_ISO3361'
 import fs from 'fs'
 
 console.log('SCRAPPING STARTED')
@@ -9,11 +9,12 @@ console.log('SCRAPPING STARTED')
 let iso3361
 let iso4717
 
+
 ISO3361CountryScrapper.scrapRemoteData(data => {
 	iso3361 = data
 	console.log("SUCCESSFUL ISO-3361 SCRAPPING!")
 	if (iso3361 && iso4717)  merge()
-	fs.writeFile("out/ISO-3361.json", JSON.stringify(data, null, 4), err => {
+	fs.writeFile("ISO-3361.json", JSON.stringify(data, null, 4), err => {
 
 			if(err) return console.log(err)
 
@@ -25,7 +26,7 @@ ISO4717CurrencyScrapper.scrapRemoteData(data => {
 	iso4717 = data
 		console.log("SUCCESSFUL ISO-4717 SCRAPPING!")
 	if (iso3361 && iso4717)  merge()
-	fs.writeFile("out/ISO-4717.json", JSON.stringify(data, null, 4), err => {
+	fs.writeFile("ISO-4717.json", JSON.stringify(data, null, 4), err => {
 
 			if(err) return console.log(err)
 
@@ -34,11 +35,11 @@ ISO4717CurrencyScrapper.scrapRemoteData(data => {
 });
 
 function merge() {
-	fs.writeFile("out/ISO-3361-merge-ISO-4717.json", JSON.stringify(ISO3361_ISO4717_Merger.merge(iso3361, iso4717), null, 4), err => {
+	fs.writeFile("ISO-3361-ISO-4717.json", JSON.stringify(ISO4717_Into_ISO3361.merge(iso3361, iso4717), null, 4), err => {
 
 			if(err) return console.log(err)
 
-			console.log("SUCCESSFUL ISO-3361 ISO-4717 MERGING!")
+			console.log("SUCCESSFUL ISO-4717 INTO ISO-3361 MERGING!")
 
 			console.log("CHECK \"out\" FOLDER")
 	});
